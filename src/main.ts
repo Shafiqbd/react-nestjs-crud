b37d6860d1861513b26a26c765c4f410f38f6a05
Shafiq <shafiq523000@gmail.com>
2023-12-18 22:58:09 +0600

    swagger implement

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest js CRUD')
+    .setDescription('This api will perform the crud operation of todos')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
